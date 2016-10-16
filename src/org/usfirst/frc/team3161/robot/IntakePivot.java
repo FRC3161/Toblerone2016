e508a5a60089782c274d3f68696b0a2ad792a19d
Andrew Azores <me@andrewazor.es>
2016-10-16 00:01:14 -0400

    Extract constants and general cleanup

--- src/org/usfirst/frc/team3161/robot/IntakePivot.java
+++ src/org/usfirst/frc/team3161/robot/IntakePivot.java
@@ -5,34 +5,24 @@ import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 public class IntakePivot extends PIDSubsystem {
 
-    public enum Position {
-        RAISED(7600),
-        LOWERED(950),
-        ;
-
-        private double value;
-
-        Position(double value) {
-            this.value = value;
-        }
-
-        public double getValue() {
-            return value;
-        }
-    }
+    public static final double KP = 0.0005d;
+    public static final double KI = 0.0d;
+    public static final double KD = 0.00075d;
 
     private static final int TOLERANCE = 150;
+    public static final double MINIMUM_MOTOR_OUTPUT = -0.85;
+    public static final double MAXIMUM_MOTOR_OUTPUT = 0.85;
 
     private CANTalon talon;
-    private Position position = Position.RAISED;
 
+    private Position position = Position.RAISED;
     public IntakePivot(CANTalon talon) {
-        super("IntakePivot", 0.0005d, 0.0d, 0.00075d);
+        super("IntakePivot", KP, KI, KD);
         this.talon = talon;
         setAbsoluteTolerance(TOLERANCE);
         getPIDController().setContinuous(false);
         setInputRange(Position.LOWERED.getValue(), Position.RAISED.getValue());
-        setOutputRange(-0.85, 0.85);
+        setOutputRange(MINIMUM_MOTOR_OUTPUT, MAXIMUM_MOTOR_OUTPUT);
         talon.enableBrakeMode(false);
         setSetpoint(position.getValue());
     }
@@ -65,6 +55,22 @@ public class IntakePivot extends PIDSubsystem {
 
     public void setPosition(Position position) {
         setSetpoint(position.getValue());
+    }
+
+    public enum Position {
+        RAISED(7600),
+        LOWERED(950),
+        ;
+
+        private double value;
+
+        Position(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
     }
 
 }
